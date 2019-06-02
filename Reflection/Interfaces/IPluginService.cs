a32e50896b389b7032574fe63f95a9b192b3c927
Emrah Atalay <emrah.atalay@outlook.com>
2019-06-02 16:25:07 +0300

    Plugin manager eklendi

---
+++ Reflection/Interfaces/IPluginService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection.Interfaces
+{
+    public interface IPluginService
+    {
+        Dictionary<string, IFilter> LoadFilters(string folder);
+    }
+}
---
+++ Reflection/Managers/PluginManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Reflection.Interfaces;
+
+namespace Reflection.Managers
+{
+    public class PluginManager : IPluginService
+    {
+        private readonly Dictionary<string, IFilter> _filters = new Dictionary<string, IFilter>();
+
+        public Dictionary<string, IFilter> LoadFilters(string folder)
+        {
+            _filters.Clear();
+            foreach (var dll in Directory.GetFiles(folder, "*.dll"))
+                try
+                {
+                    var asm = Assembly.LoadFrom(dll);
+                    foreach (var type in asm.GetTypes())
+                        if (type.GetInterface("IFilter") == typeof(IFilter))
+                        {
+                            if (Activator.CreateInstance(type) is IFilter filter)
+                                _filters[filter.Name] = filter;
+                        }
+                }
+                catch (BadImageFormatException)
+                {
+                    Console.WriteLine();
+                    throw;
+                }
+
+            return _filters;
+        }
+    }
+}
