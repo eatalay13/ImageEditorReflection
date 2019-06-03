077a097e8c98496b7d1252660f03a8fd180d7d72
Emrah Atalay <emrah.atalay@outlook.com>
2019-06-04 01:20:50 +0300

    Image Crop Plugin eklendi

---
+++ CropImage/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reflection.Interfaces;
+
+namespace CropImage
+{
+    public class CropImage : IFilter
+    {
+        public string Name => "Crop Image";
+
+        public Task<Image> RunPluginAsync(Image src)
+        {
+            return Task.Run(() => ApplyPlugin(src));
+        }
+
+        public Image RunPlugin(Image src)
+        {
+            return ApplyPlugin(src);
+        }
+
+        private Image ApplyPlugin(Image src)
+        {
+            Bitmap bmpImage = new Bitmap(src);
+            Bitmap bmpCrop = bmpImage.Clone(new Rectangle(100, 100, 300, 300), bmpImage.
+                PixelFormat);
+
+            return bmpCrop;
+        }
+    }
+}
